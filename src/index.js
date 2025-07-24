a0d318b9287a244da0c4c5bc6a6de9fdbefc9387
shakir9160 <shakir9160@gmail.com>
2025-07-24 16:54:18 +0530

    Nav bar and hero section ui implemented

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      // {
+      //   path: "/",
+      //   element: <HomePage />,
+      // },
+      // {
+      //   path: "/album/:albumId",
+      //   element: <AlbumPage />,
+      // },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
