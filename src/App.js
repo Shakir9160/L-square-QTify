6f30772672f4734a6b8cf53958d09493a5ddcd57
shakir9160 <shakir9160@gmail.com>
2025-07-25 12:16:44 +0530

    api called and getting data from api at app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import logo from "./logo.svg";
+import { StyledEngineProvider } from "@mui/material";
+import { useEffect, useState } from "react";
+import {
+  fetchFilters,
+  fetchNewAlbums,
+  fetchSongs,
+  fetchTopAlbums,
+} from "./api/api";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import { StyledEngineProvider } from "@mui/material";
-import Hero from "./components/Hero/Hero";
+import { Outlet } from "react-router-dom";
 
 function App() {
+  const [data, setData] = useState({});
+
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevState) => {
+        // Object.assign would also work
+        return { ...prevState, [key]: data };
+      });
+    });
+  };
+
+  useEffect(() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+    generateData("songs", fetchSongs);
+    generateData("genres", fetchFilters);
+  }, []);
+
+  const { topAlbums = [], newAlbums = [], songs = [], genres = [] } = data;
+
   return (
     // <div className="App">
     //   <Navbar searchData={[]} />
     // </div>
     <>
       <StyledEngineProvider injectFirst>
-        <Navbar searchData={[]} />
-        <Hero />
+        <Navbar searchData={[...topAlbums, ...newAlbums]} />
+        <Outlet context={{ data: { topAlbums, newAlbums, songs, genres } }} />
       </StyledEngineProvider>
     </>
   );
