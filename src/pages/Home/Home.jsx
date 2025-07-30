60044e06a5b38d4a122f2d849f4209b02857b374
shakir9160 <shakir9160@gmail.com>
2025-07-30 19:34:00 +0530

    home and section ui resposnive updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
+import { Divider, Stack } from "@mui/material";
 import { useOutletContext } from "react-router-dom";
 import { fetchFilters } from "../../api/api";
+import FAQs from "../../components/FAQs/FAQs";
 import Hero from "../../components/Hero/Hero";
 import Section from "../../components/Section/Section";
 import styles from "./Home.module.css";
@@ -7,23 +9,33 @@ import styles from "./Home.module.css";
 export default function Home() {
   const { data } = useOutletContext();
 
-  const { newAlbums, topAlbums, songs, genres } = data;
+  const { newAlbums, topAlbums, songs, faqs } = data;
 
   return (
     <>
       <Hero />
 
       <div className={styles.wrapper}>
-        <Section title="Top Albums" data={topAlbums} type="album" />
+        <Stack divider={<Divider flexItem />}>
+          {topAlbums && (
+            <Section title="Top Albums" data={topAlbums} type="album" />
+          )}
 
-        <Section title="New Albums" data={newAlbums} type="album" />
+          {newAlbums && (
+            <Section title="New Albums" data={newAlbums} type="album" />
+          )}
 
-        <Section
-          title="Songs"
-          data={songs}
-          filterSource={fetchFilters}
-          type="song"
-        />
+          {songs && (
+            <Section
+              title="Songs"
+              data={songs}
+              filterSource={fetchFilters}
+              type="song"
+            />
+          )}
+
+          {faqs && <FAQs faqs={faqs} />}
+        </Stack>
       </div>
     </>
   );
